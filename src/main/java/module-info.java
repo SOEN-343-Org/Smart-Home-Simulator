3b0939d396118d7c99e8e3721a79b97e586a6817
Sebastien <sebastienbn8@gmail.com>
2020-10-11 16:33:54 -0400

    Add org json dependency and export controllers modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module org.soen343 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
     opens org.soen343 to javafx.fxml;
     exports org.soen343;
+    opens org.soen343.controller to javafx.fxml;
 }
