2e1949bede062893d93e914c2628ef4a8ff21912
Sebastien <sebastienbn8@gmail.com>
2020-10-15 17:52:43 -0400

    Changed login view name to fit standard

--- src/main/java/org/soen343/controllers/LoginController.java
+++ src/main/java/org/soen343/controllers/LoginController.java
@@ -5,10 +5,6 @@ import javafx.event.ActionEvent;
 public class LoginController extends Controller {
     DashboardController mainController;
 
-    public void setMain(DashboardController c) {
-        mainController = c;
-    }
-
     public void changeScene(ActionEvent actionEvent) {
         mainController.exitLoginView();
     }
