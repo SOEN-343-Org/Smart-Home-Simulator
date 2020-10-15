951aa4ec39c4ef214f40712e2d73d725bd0b4c5f
TonyMontana11 <anth.chraim@gmail.com>
2020-10-15 15:19:24 -0400

    Start app from login

--- src/main/java/org/soen343/controllers/LoginController.java
+++ src/main/java/org/soen343/controllers/LoginController.java
@@ -1,9 +1,18 @@
 package org.soen343.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class LoginController extends Controller {
     DashboardController mainController;
+
+    @FXML
+    TextField signUpUsername;
+    @FXML
+    TextField signUpPassword;
+    @FXML
+    TextField signUpButton;
 
     public void changeScene(ActionEvent actionEvent) {
         mainController.exitLoginView();
