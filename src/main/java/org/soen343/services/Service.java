1455f2c8016df28e3af34ad632e9179d641c861f
Sebastien <sebastienbn8@gmail.com>
2020-10-16 17:55:02 -0400

    Added Service abstract class

---
+++ src/main/java/org/soen343/services/Service.java
@@ -0,0 +1,47 @@
+package org.soen343.services;
+
+import org.soen343.models.Individual;
+import org.soen343.models.Model;
+import org.soen343.models.Room;
+
+import java.util.ArrayList;
+
+public abstract class Service {
+
+    public static Model model = new Model();
+
+    public Service() {
+
+    }
+
+    public boolean SimulationIsRunning() {
+        return model.simulationRunning;
+    }
+
+    public ArrayList<String> getHouseRoomsName() {
+        return model.house.roomsName;
+    }
+
+    public String getCurrentUserLocation() {
+        return model.user.getCurrentIndividual().getLocation();
+    }
+
+    public ArrayList<Individual> getIndividuals() {
+        return model.house.getIndividuals();
+    }
+
+    public ArrayList<Room> getHouseRooms() {
+        return model.house.getRooms();
+    }
+
+    public Individual getCurrentUserIndividual() {
+        return model.user.getCurrentIndividual();
+    }
+
+    public void updateIndividualLocation(Individual individual, String location) {
+        //TODO: Log that we update location of that individual
+        individual.setLocation(location);
+    }
+
+
+}
