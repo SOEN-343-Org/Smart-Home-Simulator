97b8f0050edb02cb4f80c52a2e431e2d8b73915d
WanProduction <48234317+WanProduction@users.noreply.github.com>
2020-10-18 14:07:41 -0400

    DateTime documentation

--- src/main/java/org/soen343/models/DateTime.java
+++ src/main/java/org/soen343/models/DateTime.java
@@ -3,6 +3,7 @@ package org.soen343.models;
 import java.time.LocalDate;
 
 public class DateTime {
+
     private int hours;
     private int minutes;
     private int seconds;
@@ -11,38 +12,78 @@ public class DateTime {
     public DateTime() {
     }
 
+    /**
+     *Get hours
+     *
+     * @return int hours
+     */
     public int getHours() {
         return hours;
     }
 
+    /**
+     * Set hours
+     *
+     * @param hours
+     */
     public void setHours(int hours) {
         if (0 <= hours && hours <= 23) {
             this.hours = hours;
         }
     }
 
+    /**
+     * Get hours
+     *
+     * @return int minutes
+     */
     public int getMinutes() {
         return minutes;
     }
 
+    /**
+     * Set minutes
+     *
+     * @param minutes
+     */
     public void setMinutes(int minutes) {
         if (0 <= minutes && minutes <= 59) {
             this.minutes = minutes;
         }
     }
 
+    /**
+     * Get date
+     *
+     * @return int date
+     */
     public LocalDate getDate() {
         return date;
     }
 
+    /**
+     * Set date
+     *
+     * @param date
+     */
     public void setDate(LocalDate date) {
         this.date = date;
     }
 
+    /**
+     * Get seconds
+     *
+     * @return int seconds
+     */
     public int getSeconds() {
         return seconds;
     }
 
+    /**
+     * Set seconds
+     *
+     * @param seconds
+     */
     public void setSeconds(int seconds) {
         if (0 <= seconds && seconds <= 59) {
             this.seconds = seconds;
