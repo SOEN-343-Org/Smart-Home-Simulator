07b54948c4c93abd27df21fdda1b0fb01c83f8e1
Sebastien <sebastienbn8@gmail.com>
2020-11-04 19:41:23 -0500

    colored text

--- src/main/java/org/soen343/controllers/ConsoleOutputController.java
+++ src/main/java/org/soen343/controllers/ConsoleOutputController.java
@@ -1,25 +1,44 @@
 package org.soen343.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.soen343.services.ConsoleOutputService;
 
 public class ConsoleOutputController extends Controller {
+
     @FXML
-    private TextArea console;
+    private TextFlow outputArea;
 
     private ConsoleOutputService consoleOutputService;
 
     void initializeController() {
         consoleOutputService = ConsoleOutputService.getInstance();
+
     }
 
     @Override
     void update() {
-        String log = consoleOutputService.getLog();
+        String log = consoleOutputService.getLog() + "\n";
         String level = consoleOutputService.getLevel();
-        String oldLog = console.getText();
-        console.appendText("\n" + log);
+        Text t = new Text(log);
+        selectTextColor(level, t);
+        outputArea.getChildren().add(t);
+    }
+
+    private void selectTextColor(String level, Text t) {
+        switch (level) {
+            case "INFO":
+                t.setFill(Color.BLACK);
+                break;
+            case "CRITICAL":
+                t.setFill(Color.RED);
+                break;
+            case "ERROR":
+                t.setFill(Color.ORANGE);
+                break;
+        }
     }
 
 
