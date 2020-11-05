53aee6e9ea77c4e357a8492f5341ac79cd25cd28
Sebastien <sebastienbn8@gmail.com>
2020-11-04 19:56:28 -0500

    Fixed scrolling

--- src/main/java/org/soen343/controllers/ConsoleOutputController.java
+++ src/main/java/org/soen343/controllers/ConsoleOutputController.java
@@ -1,21 +1,35 @@
 package org.soen343.controllers;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import org.soen343.services.ConsoleOutputService;
 
 public class ConsoleOutputController extends Controller {
-
+    @FXML
+    private ScrollPane scrollPane;
     @FXML
     private TextFlow outputArea;
 
     private ConsoleOutputService consoleOutputService;
 
     void initializeController() {
+        outputArea = new TextFlow();
+        outputArea.setStyle("-fx-background-color: #FFFFFF;");
+        outputArea.setPrefWidth(scrollPane.getPrefWidth());
         consoleOutputService = ConsoleOutputService.getInstance();
-
+        outputArea.getChildren().addListener(
+                (ListChangeListener<Node>) ((change) -> {
+                    outputArea.layout();
+                    scrollPane.layout();
+                    scrollPane.setVvalue(1.0f);
+                }));
+        scrollPane.setContent(outputArea);
     }
 
     @Override
@@ -23,6 +37,7 @@ public class ConsoleOutputController extends Controller {
         String log = consoleOutputService.getLog() + "\n";
         String level = consoleOutputService.getLevel();
         Text t = new Text(log);
+        t.setFont(new Font(14));
         selectTextColor(level, t);
         outputArea.getChildren().add(t);
     }
