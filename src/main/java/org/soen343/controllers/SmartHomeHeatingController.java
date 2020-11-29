49115e463bf107bb075584cd686151ff96d1c9b4
Sebastien <sebastienbn8@gmail.com>
2020-11-29 16:30:37 -0500

    Fix database

---
+++ src/main/java/org/soen343/controllers/SmartHomeHeatingController.java
@@ -0,0 +1,15 @@
+package org.soen343.controllers;
+
+
+public class SmartHomeHeatingController extends Controller {
+
+    @Override
+    void initializeController() {
+
+    }
+
+    @Override
+    void update() {
+
+    }
+}
---
+++ src/main/java/org/soen343/services/modules/SHHModule.java
@@ -0,0 +1,8 @@
+package org.soen343.services.modules;
+
+import org.soen343.services.Service;
+
+public class SHHModule extends Service {
+
+
+}
