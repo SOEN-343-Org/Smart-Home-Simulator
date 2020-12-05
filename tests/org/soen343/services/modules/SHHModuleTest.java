4d87fce9400ed7eb940df7922e5eb20394b5ac76
TonyMontana11 <anth.chraim@gmail.com>
2020-12-04 23:39:39 -0500

    fixed bug in tests and changed its location

--- tests/org/soen343/services/modules/SHHModuleTest.java
+++ tests/org/soen343/services/modules/SHHModuleTest.java
@@ -1,13 +1,17 @@
-package test;
+package org.soen343.services.modules;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.soen343.models.house.Light;
-import org.soen343.models.house.Room;
 import org.soen343.models.house.Zone;
+import org.soen343.models.house.Room;
 
-public class settingZoneTest {
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SHHModuleTest {
+
     private Zone zone;
-    private Room room;
 
     @Test
     public void createZoneAndCheckIfNotNull(){
@@ -23,17 +27,15 @@ public class settingZoneTest {
     @Test
     public void addRoomInZoneCheckIfZoneContainsRoom(){
         zone= new Zone("zone1");
-        Light [] lights= new Light[2];
+        Light[] lights= new Light[2];
         lights[0]= new Light(1);
         lights[1]= new Light(2);
         Object top= new Object();
         Object right= new Object();
         Object down= new Object();
         Object left= new Object();
-        room= new Room(4, "Kitchen",lights, top, right, down, left);
+        Room room= new Room(4, "Kitchen",lights, top, right, down, left);
         zone.addRoom(room);
         Assertions.assertTrue(zone.getRooms().contains(room));
     }
-
-
 }
